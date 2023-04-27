7cb84fcc3a9ba7ad47545a8774b6256154c52515
Andelf <andelf@gmail.com>
2023-04-27 21:36:09 +0800

    chore: enbale trace all

--- e2e-tests/whiteboards.spec.ts
+++ e2e-tests/whiteboards.spec.ts
@@ -83,6 +83,7 @@ test('draw a rectangle', async ({ page }) => {
   const canvas = await page.waitForSelector('.logseq-tldraw')
   const bounds = (await canvas.boundingBox())!
 
+  await page.waitForTimeout(1000)
   await page.keyboard.press('r')
 
   await page.mouse.move(bounds.x + 5, bounds.y + 5)
